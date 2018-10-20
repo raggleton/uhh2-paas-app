8daa12337923cda724542a17ca75ea81a203f355
Robin Aggleton <robin.aggleton@cern.ch>
2018-10-20 22:38:01 +0200

    Add reminder about filename

--- myapp.py
+++ myapp.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
-"""App to listen for github events, and carry out a corresponding task e.g. push to gitlab"""
+"""App to listen for github events, and carry out a corresponding task e.g. push to gitlab
+
+NB if you rename this file to `app.py` in the future, you'll need to set 
+`APP_FILE` in openshift to null otherwise it'll run this instead of gunicorn:
+https://github.com/sclorg/s2i-python-container/issues/190
+"""
 
 
 from flask import Flask
